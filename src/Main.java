b424d5430a49ed83b6cae73c42ed7a16e0017191
borishdez <bhernandez@bitworks.com.sv>
2024-09-13 18:09:10 -0600

    cast (int)

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,8 @@ public class Main {
                 Fué lanzada en:
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        System.out.println(clasificacion);
     }
 }
