fd0e19e3e2c0b812006165708ce232db92f1ddf7
borishdez <bhernandez@bitworks.com.sv>
2024-09-13 19:07:37 -0600

    Desafio - opciones switch case

--- src/Desafio.java
+++ src/Desafio.java
@@ -24,6 +24,33 @@ public class Desafio {
         while (opcion != 9){
             System.out.println(menu);
             opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("El saldo actualizado es: " + saldo + "$");
+                    break;
+                case 2:
+                    System.out.println("¿Cuál es el valor que desea retirar?");
+                    double valorARetirar = teclado.nextDouble();
+                    if (valorARetirar > saldo){
+                        System.out.println("Saldo insuficiente");
+                    }else {
+                        saldo -= valorARetirar;
+                        System.out.println("El saldo actualizado es: " + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("¿Cuál es el valor que desea depositar?");
+                    double valorADepositar = teclado.nextDouble();
+                    saldo += valorADepositar;
+                    System.out.println("El saldo actualizado es: " + saldo);
+                    break;
+                case 9:
+                    System.out.println("Saliendo del programa, gracias por utilizar nuestros servicios!");
+                    break;
+                default:
+                    System.out.println("Opción no valida");
+            }
         }
     }
 }
